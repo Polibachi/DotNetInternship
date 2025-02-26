c63420f14239d2e3d3f8c75a5ffec5cf05537280
Nagrein <nagreinstudy@gmail.com>
2025-02-26 18:05:12 +0200

    Add notification page

--- ServiceStaff/servicestaff.client/src/app/app-routing.module.ts
+++ ServiceStaff/servicestaff.client/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { OrdersComponent } from './pages/orders/orders.component';
 import { KitchenComponent } from './pages/kitchen/kitchen.component';
 import { OrderListComponent } from './pages/order-list/order-list.component';
 import { OrderItemComponent } from './pages/order-item/order-item.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
 
 import { AuthGuard } from './guards/auth.guard'; // ✅ Імпортуємо Guard
 
@@ -19,7 +20,8 @@ const routes: Routes = [
   { path: 'orders', component: OrdersComponent, canActivate: [AuthGuard] },
   { path: 'kitchen', component: KitchenComponent, canActivate: [AuthGuard] },
   { path: 'order-list', component: OrderListComponent, canActivate: [AuthGuard] },
-  { path: 'order-item', component: OrderItemComponent, canActivate: [AuthGuard] }
+  { path: 'order-item', component: OrderItemComponent, canActivate: [AuthGuard] },
+  { path: 'notifications', component: NotificationsComponent, canActivate: [AuthGuard] }
 ];
 
 
