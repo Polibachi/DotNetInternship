cb13a8cd1b68b7a1002fca30567dd5f05a556d3c
VasyaFanta <vasyafanta76@gmail.com>
2025-02-26 14:26:29 +0200

    Revert "Merge pull request #7 from Polibachi/KAN-19-Back"
    This reverts commit e00ebd8bd1d4847979fdc19d4aecad2f03cc3abd, reversing
    changes made to 268c951d29acfaa1fd2e5d1a6fd4bd2c41510385.

--- ServiceStaff/ServiceStaff.Server/Program.cs
+++ ServiceStaff/ServiceStaff.Server/Program.cs
@@ -10,18 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSignalR();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy",
-        builder => builder
-            .WithOrigins("http://localhost:4200")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials()
-            .SetIsOriginAllowed(host => true));
-});
 
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
@@ -66,6 +58,8 @@ builder.Services.AddAuthentication(x =>
 });
 
 var app = builder.Build();
+
+app.UseDefaultFiles();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- ServiceStaff/ServiceStaff.Server/Services/OrderService.cs
+++ ServiceStaff/ServiceStaff.Server/Services/OrderService.cs
@@ -1,5 +1,4 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using ServiceStaff.Server.Data;
 using ServiceStaff.Server.DTO;
@@ -34,10 +33,6 @@ namespace ServiceStaff.Server.Services
             await _context.SaveChangesAsync();
             return "Order created successfully";
         }
-
-        // await _hubContext.Clients.User(order.CookId).SendAsync("ReceiveNotification", $"Нове замовлення #{order.Id}!");
-        // надсилання сповіщень
-
 
         public async Task<string> CompleteOrderAsync(int orderId)
         {
