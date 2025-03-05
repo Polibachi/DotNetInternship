7a303a1cc87978074fb76405e7a9fed635365e56
Ivan1uk <mivanuk559@gmail.com>
2025-03-05 21:48:19 +0200

    Картка для додавання

--- ServiceStaff/servicestaff.client/src/app/pages/menu/menu.component.ts
+++ ServiceStaff/servicestaff.client/src/app/pages/menu/menu.component.ts
@@ -10,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
 import { DishService } from '../../services/dish.service';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { AddDishDialogComponent } from '../add-dish-dialog/add-dish-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 interface Dish {
   id: number;
   name: string;
@@ -37,16 +39,38 @@ interface Dish {
 export class MenuComponent implements OnInit {
   dishes: Dish[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private dishService: DishService, private dialog: MatDialog, private http: HttpClient) { }
 
-  ngOnInit() {
-    this.http.get<Dish[]>('https://localhost:7155/api/Dish/all')
-      .subscribe((data) => {
-        this.dishes = data;
-      });
+  ngOnInit(): void {
+    this.loadDishes();
+  }
+
+  loadDishes(): void {
+    this.dishService.getDishes().subscribe((data) => {
+      this.dishes = data;
+    });
+  }
+
+  openAddDishDialog(): void {
+    const dialogRef = this.dialog.open(AddDishDialogComponent, {
+      width: '800px', // Оптимізований розмір
+      height: '600px'
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.loadDishes();
+      }
+    });
+  }
+  // Додаємо властивість для відображення останнього ID + 1
+  getLastDishIdPlusOne(): number {
+    const lastDish = this.dishes[this.dishes.length - 1];
+    return lastDish ? lastDish.id + 1 : 1; // Якщо страви є, додаємо 1 до останнього ID, якщо немає, то виводимо 1
   }
 
   getDishImage(id: number): string {
     return `assets/images/dishes/${id}.jpeg`;
   }
 }
+
