6d1dc7eead6aa434c793fbdac730273298a252c7
Ivan1uk <mivanuk559@gmail.com>
2025-03-13 13:55:43 +0200

    Update

--- ServiceStaff/servicestaff.client/src/app/pages/header/header.component.ts
+++ ServiceStaff/servicestaff.client/src/app/pages/header/header.component.ts
@@ -11,6 +11,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
 
 @Component({
+
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
